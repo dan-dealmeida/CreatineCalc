07d8edbbd3c602fb9f8c09e0dce18f0347660caf
Daniel Antunes de Almeida <danielalmeidarj01@gmail.com>
2025-02-13 21:35:48 -0300

    kp

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 function calculateCreatine() {
     let weight = parseFloat(document.getElementById("weight").value);
 
-    if (isNaN(weight) || weight <= 0 ) {
-        alert("Por favor, insira valores válidos para peso.");
+    if (isNaN(weight) || weight <= 0) {
+        alert("Por favor, insira um valor válido para o peso.");
         return;
     }
 
@@ -10,8 +10,11 @@ function calculateCreatine() {
 
     // Ajuste da dosagem com base no nível de atividade
     creatineDose *= 1.2; // Aumenta 20% para pessoas muito ativas
+    let loadingDose = creatineDose * 4; // Correção do nome da variável
 
     let resultText = `Você deve tomar aproximadamente <span>${creatineDose.toFixed(2)}</span> gramas de creatina por dia.`;
+    let resultText2 = `Caso opte pela fase de saturação, tome aproximadamente <span>${loadingDose.toFixed(2)}</span> gramas de creatina por dia nos primeiros dias.`;
 
     document.getElementById("result").innerHTML = resultText;
+    document.getElementById("result2").innerHTML = resultText2;
 }
